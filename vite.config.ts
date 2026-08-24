dac6db6f16b786e8d9dd52cf164756d67e91559c
ibrahem khaled <xx.vvv48@gmail.com>
2026-08-24 18:35:53 +0300

    fix(deploy): update base path to relative for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Ibrahem-Khaled.github.io/',
+  base: './',
   plugins: [react(), tailwindcss()],
 })
